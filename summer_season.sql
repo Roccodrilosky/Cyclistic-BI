60a0ca99720e51ecf709f3bd642db62c0aa30a8e
Roccodrilosky <lrgarciaq@gmail.com>
2024-10-09 18:21:29 -0600

    Update summer_season.sql

--- summer_season.sql
+++ summer_season.sql
@@ -1,5 +1,9 @@
 SELECT
   TRI.usertype,
+  TRI.start_station_longitude,
+  TRI.start_station_latitude,
+  TRI.end_station_longitude,
+  TRI.end_station_latitude,
   ZIPSTART.zip_code AS zip_code_start,
   ZIPSTARTNAME.borough borough_start,
   ZIPSTARTNAME.neighborhood AS neighborhood_start,
@@ -12,30 +16,30 @@ SELECT
   WEA.wdsp AS day_mean_wind_speed,
   WEA.prcp day_total_precipitation,
   ROUND(CAST(TRI.tripduration / 60 AS INT64), -1) AS trip_minutes,
-  COUNT(TRI.bikeid) AS trip_count
+  TRI.bikeid
 FROM
   `bigquery-public-data.new_york_citibike.citibike_trips` AS TRI
 INNER JOIN
   `bigquery-public-data.geo_us_boundaries.zip_codes` ZIPSTART
-  ON ST_WITHIN(
-    ST_GEOGPOINT(TRI.start_station_longitude, TRI.start_station_latitude),
-    ZIPSTART.zip_code_geom)
+ON
+  ST_WITHIN( ST_GEOGPOINT(TRI.start_station_longitude, TRI.start_station_latitude), ZIPSTART.zip_code_geom)
 INNER JOIN
   `bigquery-public-data.geo_us_boundaries.zip_codes` ZIPEND
-  ON ST_WITHIN(
-    ST_GEOGPOINT(TRI.end_station_longitude, TRI.end_station_latitude),
-    ZIPEND.zip_code_geom)
+ON
+  ST_WITHIN( ST_GEOGPOINT(TRI.end_station_longitude, TRI.end_station_latitude), ZIPEND.zip_code_geom)
 INNER JOIN
   `bigquery-public-data.noaa_gsod.gsod20*` AS WEA
-  ON PARSE_DATE("%Y%m%d", CONCAT(WEA.year, WEA.mo, WEA.da)) = DATE(TRI.starttime)
+ON
+  PARSE_DATE("%Y%m%d", CONCAT(WEA.year, WEA.mo, WEA.da)) = DATE(TRI.starttime)
 INNER JOIN
-  `(insert your table name) zipcodes` AS ZIPSTARTNAME
-  ON ZIPSTART.zip_code = CAST(ZIPSTARTNAME.zip AS STRING)
+  `pragmatic-zoo-437820-p1.cyclist.nyc_zip_codes` AS ZIPSTARTNAME
+ON
+  ZIPSTART.zip_code = CAST(ZIPSTARTNAME.zip AS STRING)
 INNER JOIN
-  `(insert your table name) zipcodes` AS ZIPENDNAME
-  ON ZIPEND.zip_code = CAST(ZIPENDNAME.zip AS STRING)
+  `pragmatic-zoo-437820-p1.cyclist.nyc_zip_codes` AS ZIPENDNAME
+ON
+  ZIPEND.zip_code = CAST(ZIPENDNAME.zip AS STRING)
 WHERE
   WEA.wban = '94728'
-  AND EXTRACT(YEAR FROM DATE(TRI.starttime)) BETWEEN 2014 AND 2015
-GROUP BY
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+  AND DATE(TRI.starttime) BETWEEN DATE('2015-07-01')
+  AND DATE('2015-09-30')
