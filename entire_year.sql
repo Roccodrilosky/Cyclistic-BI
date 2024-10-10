af09fd65c11034a2774910ecff17d67ab64df792
Roccodrilosky <lrgarciaq@gmail.com>
2024-10-09 18:15:14 -0600

    Update entire_year.sql

--- entire_year.sql
+++ entire_year.sql
@@ -29,10 +29,10 @@ INNER JOIN
   `bigquery-public-data.noaa_gsod.gsod20*` AS WEA
   ON PARSE_DATE("%Y%m%d", CONCAT(WEA.year, WEA.mo, WEA.da)) = DATE(TRI.starttime)
 INNER JOIN
-  `(insert your table name) zipcodes` AS ZIPSTARTNAME
+  `pragmatic-zoo-437820-p1.cyclist.nyc_zip_codes` AS ZIPSTARTNAME
   ON ZIPSTART.zip_code = CAST(ZIPSTARTNAME.zip AS STRING)
 INNER JOIN
-  `(insert your table name) zipcodes` AS ZIPENDNAME
+  `pragmatic-zoo-437820-p1.cyclist.nyc_zip_codes` AS ZIPENDNAME
   ON ZIPEND.zip_code = CAST(ZIPENDNAME.zip AS STRING)
 WHERE
   WEA.wban = '94728'
